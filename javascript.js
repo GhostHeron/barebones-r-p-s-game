a09a4614bf1e6a316f9a1fd6818fd4fd971e1ccd
GhostHeron <micahsws0@gmail.com>
2022-12-10 11:09:10 +0100

    Make new player input system
    Make a new player input system, instead of inputting 0 1 or 2
    players can now type rock paper or scissors. Make if block less
    horrible.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
 
-const playerChoice = prompt('input 0 for rock, 1 for paper, and 2 for scissors.');
+const playerInput = prompt('choose rock, paper, or scissors.');
 
-const computerChoice = Math.floor(Math.random() * 3);
+const computerChoice = Math.floor(Math.random() * 3); //finds random integer from zero to two
 
-let choiceDisplay = choiceDisplayInput();
+let choiceDisplay = choiceDisplayInput(); //variable to store output of choiceDisplayInput()
 
+// converts computer choice into strings
 function choiceDisplayInput() {
-if (computerChoice == '0') {
+if (computerChoice == 0) {
     return 'rock';
 } else if (computerChoice == 1){
     return 'paper';
@@ -17,25 +18,43 @@ if (computerChoice == '0') {
 }
 }
 
+//sets the whole string inputted by the player to lowercase so it works in playerChoiceConver()
+let lowercasePlayerInput = playerInput;
+lowercasePlayerInput = lowercasePlayerInput.toLowerCase()
 
-if (playerChoice == 0 && computerChoice == 0) {
-    output = `the computer chose ${choiceDisplay}. A tie! wow.`;
+let playerChoice = playerChoiceConvert()
+
+//converts player's choice into a number so it can be compared
+function playerChoiceConvert() {
+    if (lowercasePlayerInput == 'rock') {
+        return 0;
+    } else if (lowercasePlayerInput == 'paper') {
+        return 1;
+    } else if (lowercasePlayerInput == 'scissors') {
+        return 2;
+    }
+}
+
+//variables for different game outcomes
+const win = `the computer chose ${choiceDisplay}. You won!`;
+const tie = `the computer chose ${choiceDisplay}. A tie! wow.`;
+const loss = `the computer chose ${choiceDisplay}. You lost. Better luck next time!`;
+
+//if else block that determines game outcome.
+if (playerChoice == computerChoice) {
+    output = tie;
 } else if (playerChoice == 0 && computerChoice == 1){
-    output = `the computer chose ${choiceDisplay}. You lost. Better luck next time!`;
+    output = loss;
 } else if (playerChoice == 0 && computerChoice == 2) {
-    output = `the computer chose ${choiceDisplay}. You won!`;
+    output = win;
 } else if (playerChoice == 1 && computerChoice == 0) {
-    output = `the computer chose ${choiceDisplay}. You won!`;
-} else if (playerChoice == 1 && computerChoice == 1) {
-    output = `the computer chose ${choiceDisplay}. A tie! wow.`;
+    output = win;
 } else if (playerChoice == 1 && computerChoice == 2) {
-    output = `the computer chose ${choiceDisplay}. You lost. Better luck next time!`;
+    output = loss
 } else if (playerChoice == 2 && computerChoice == 0) {
-    output = `the computer chose ${choiceDisplay}. You lost. Better luck next time!`;
+    output = loss;
 } else if (playerChoice == 2 && computerChoice == 1) {
-    output = `the computer chose ${choiceDisplay}. You won!`;
-} else if (playerChoice == 2 && computerChoice == 2) {
-    output = `the computer chose ${choiceDisplay}. A tie! wow.`;
+    output = win;
 }else {
     output = "something went wrong.";
 }
